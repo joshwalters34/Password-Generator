eb79a81c1e178b7df535dced02ee834f057da48c
JWalters <joshwalters34@gmail.com>
2021-02-05 09:05:55 -0500

    Added functions to call the prompts that the user will see.

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,50 @@ function writePassword() {
 
 }
 
+var button = document.getElementById("generate");
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+document.getElementById("generate").addEventListener("click", charLength);
+document.getElementById("generate").addEventListener("click", charLowerCase);
+document.getElementById("generate").addEventListener("click", charUpperCase);
+document.getElementById("generate").addEventListener("click", charNumber);
+document.getElementById("generate").addEventListener("click", charSpecial);
+
+//button.addEventListener("click", charLength);
+
+function charLength () {
+  window.prompt ("Choose a length between 8 and 16 characters.");
+  var characterLength = document.getElementById("characterLength").value;
+  if (characterLength <8) {
+    alert ("Password must contain at least 8 characters.")
+    window.prompt ("Choose a password length between 8 and 16 characters.")
+  }
+}
+
+
+function charLowerCase() {
+  window.confirm("Include a lowercase letter in your password?");
+}
+
+function charUpperCase() {
+  window.confirm ("Include an uppercase letter in your password?");
+}
+
+function charNumber() {
+  window.confirm ("Include a number in your password?");
+}
+
+function charSpecial() {
+  window.confirm ("Include a special character in your password?");
+}
+
+//var special = [!@#$%^&*];
+
+//prompts for each value of the password
+//var charLength = window.prompt ("Choose a length between 8 and 16 characters.");
+//var charLowerCase = window.prompt ("Include a lowercase character in your password?");
+//var charUpperCase = window.prompt ("Include an uppercase character in your password?");
+//var charSpecial = window.prompt ("Include a special character in your password?");
+//var charNumber = window.prompt ("Include a number in your password?");
